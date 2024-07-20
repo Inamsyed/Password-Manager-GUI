e0f61d1496de0ad7a246b6ca2be0eab6bd507108
Inam Syed <inamsyed2020@gmail.com>
2024-07-20 14:23:44 +0100

    Initial commit

---
+++ extra.py
@@ -0,0 +1,19 @@
+import random
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+list1 = [random.choice(letters) for x in range(nr_letters)]
+list2 = [random.choice(numbers) for x in range(nr_numbers)]
+list3 = [random.choice(symbols) for x in range(nr_symbols)]
+password_list = list1+list2+list3
+print(list1)
+print(list2)
+print(list3)
+print(password_list)
---
+++ main.py
@@ -0,0 +1,115 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    list1 = [random.choice(letters) for x in range(nr_letters)]
+    list2 = [random.choice(numbers) for x in range(nr_numbers)]
+    list3 = [random.choice(symbols) for x in range(nr_symbols)]
+    password_list = list1 + list2 + list3
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    pw_entry.delete(0, END)
+    pw_entry.insert(0,password)
+    pyperclip.copy(password)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save():
+    website = web_entry.get()
+    email = email_entry.get()
+    password = pw_entry.get()
+
+    if(len(website) == 0 or len(password) == 0):
+        messagebox.showinfo(title="Error", message="Cannot leave any field empty.")
+    else:
+        is_ok = messagebox.askokcancel(title="Confirmation", message='Are the details ok to save?')
+
+        if(is_ok == True):
+            with open("data.txt", "a") as file:
+                file.write(f"Website: {website} | Email: {email} | Password: {password}\n")
+                web_entry.delete(0, 'end')
+                pw_entry.delete(0, 'end')
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+# Main window
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+# Canvas containing the actual image
+canvas = Canvas()
+# canvas["bg"] = "yellow"
+canvas["width"] = 200
+canvas["height"] = 200
+logo_img = PhotoImage(file="./logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+# Labels
+website_label = Label()
+website_label["text"] = "Website:"
+website_label.grid(row=1, column=0)
+
+email_label = Label()
+email_label["text"] = "Email/Username:"
+email_label.grid(row=2, column=0)
+
+pw_label = Label()
+pw_label["text"] = "Password:"
+pw_label.grid(row=3, column=0)
+
+# Entries
+web_entry = Entry()
+web_entry["width"] = 52
+web_entry.focus()
+web_entry.grid(row=1, column=1, columnspan=2)
+
+email_entry = Entry()
+email_entry["width"] = 52
+email_entry.insert(0, "inamsyed2020@gmail.com")
+email_entry.grid(row=2, column=1, columnspan=2)
+
+pw_entry = Entry()
+pw_entry["width"] = 33
+pw_entry.grid(row=3, column=1)
+
+# Buttons
+pw_button = Button()
+pw_button["text"] = "Generate Password"
+pw_button["width"] = 14
+pw_button["command"] = generate_password
+pw_button.grid(row=3, column=2)
+
+add_button = Button()
+add_button["text"] = "Add"
+add_button["width"] = 44
+add_button["command"] = save
+add_button.grid(row=4, column=1, columnspan=2)
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
